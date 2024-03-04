9a42b59d94b7e315dae4bae8fbd748d37b003851
MUSA YILMAZ <yilmazmusa66@gmail.com>
2024-03-05 01:11:13 +0300

    Stock.API oluşturuldu. MongoDBDatabase i konfigure edildi, dummy datalar ile  Stock collectionu  MongoDB ye eklendi.

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,4 +1,6 @@
-using MassTransit;
+﻿using MassTransit;
+using MongoDB.Driver;
+using Stock.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +19,50 @@ builder.Services.AddMassTransit(configurator =>
     });
 });
 
+builder.Services.AddSingleton<MongoDBService>(); //Oluşturduğumuz MongoDBService'i IOC Container a ekledik.
+
 var app = builder.Build();
+
+using IServiceScope scope = builder.Services.BuildServiceProvider().CreateScope(); //Bir defa kullanılıp sonra dispose edileceği için using kullandık.
+MongoDBService mongoDBService = scope.ServiceProvider.GetRequiredService<MongoDBService>();
+
+// Bu kontrol Stock.API ayağa kalktığında MongoDB inde hem Stock Collectionunu oluşturacak ve  Stock Collection'unda data yoksa if bloğunun içindeki dataları ekleyecek,
+// varsa eklemeyecek  zaten.
+if (! await (await mongoDBService.GetCollection<Stock.API.Models.Stock>().FindAsync(x => true)).AnyAsync()) 
+{
+    mongoDBService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 1,
+        ProductName = "Elma",
+        Count = 200,
+    });
+    mongoDBService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 2,
+        ProductName = "Armut",
+        Count = 300,
+    });
+    mongoDBService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 3,
+        ProductName = "Muz",
+        Count = 50,
+    });
+    mongoDBService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 4,
+        ProductName = "Avakado",
+        Count = 10,
+    });
+    mongoDBService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 5,
+        ProductName = "Çilek",
+        Count = 60,
+    });
+}
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Stock.API/Services/MongoDBService.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace Stock.API.Services
+{
+    public class MongoDBService
+    {
+        readonly IMongoDatabase mongoDatabase;
+        public MongoDBService(IConfiguration configuration) //Bir servis ya da sınıf içerisinden appsettings.json daki bir veriyi getirebilmek için IConfiguration dan yararlarınız.
+        {
+            MongoClient client = new(configuration.GetConnectionString("MongoDB"));
+            mongoDatabase = client.GetDatabase("StockDB2");
+        }
+
+        public IMongoCollection<T> GetCollection<T>() => mongoDatabase.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
+    }
+}
